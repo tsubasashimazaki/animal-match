1b3265741731178e015e959e1f2b7a0def9a403a
github tsubasashimazaki <github>
2020-05-31 12:27:23 +0900

    fontawesome,sass読み込み

---
+++ resources/views/top.blade.php
@@ -0,0 +1,13 @@
+<!-- layoutファイルの読み込み -->
+@extends('layouts.app')
+
+@section('content')<!-- それぞれのファイル内容ここから -->
+<div class="loginPage">
+  <div class="container">
+    <div class="loginPage_contents">
+    <h1 class="h3 loginPage_contents_title">新しいマッチを見つけよう</h1>
+    <div class="btn loginPage_contents_btn"><a class="text-white" href="{{ route('login') }}">メールアドレスでログインする</a></div>
+    </div>
+  </div>
+</div>
+@endsection<!-- ここまで -->
